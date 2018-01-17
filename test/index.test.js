9410fd13ce5ef47b90f91bb9af363557614b360b
Knut Hühne <knut.huehne@correctiv.org>
2018-01-17 16:39:30 +0100

    add option to exclude layers from translation
    Implements #7

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var MapboxLanguage = require('../index');
 
-function makeStyle(layer) {
+function makeStyle(layers) {
   return {
     sources: {
       composite: {
@@ -9,15 +9,13 @@ function makeStyle(layer) {
         url: 'mapbox://mapbox-streets-v7'
       }
     },
-    layers: [layer]
+    layers: layers
   };
-
-
 }
 
 test('setLanguage for different text fields', (assert) => {
   var language = new MapboxLanguage();
-  var style = makeStyle({
+  var style = makeStyle([{
     'id': 'state-label-sm',
     'source': 'composite',
     'source-layer': 'state_label',
@@ -31,7 +29,7 @@ test('setLanguage for different text fields', (assert) => {
         ]
       }
     }
-  });
+  }]);
 
   var enStyle = language.setLanguage(style, 'es');
   assert.deepEqual(enStyle.layers[0].layout, {
@@ -57,5 +55,62 @@ test('setLanguage for different text fields', (assert) => {
     }
   }, 'switch style to spanish arabic field');
 
+  assert.end();
+});
+
+test('setLanguage with excluded layers', (assert) => {
+  var language = new MapboxLanguage({excludedLayerIds: ['state-label-lg']});
+  var style = makeStyle([{
+    'id': 'state-label-sm',
+    'source': 'composite',
+    'source-layer': 'state_label',
+    'layout': {
+      'text-letter-spacing': 0.15,
+      'text-field': {
+        'base': 1,
+        'stops': [
+          [0, '{abbr}'],
+          [6, '{name}']
+        ]
+      }
+    }
+  }, {
+    'id': 'state-label-lg',
+    'source': 'composite',
+    'source-layer': 'state_label',
+    'layout': {
+      'text-letter-spacing': 0.15,
+      'text-field': {
+        'base': 1,
+        'stops': [
+          [0, '{abbr}'],
+          [6, '{name}']
+        ]
+      }
+    }
+  }]);
+
+  var esStyle = language.setLanguage(style, 'es');
+  assert.deepEqual(esStyle.layers[0].layout, {
+    'text-letter-spacing': 0.15,
+    'text-field': {
+      'base': 1,
+      'stops': [
+        [0, '{abbr}'],
+        [6, '{name_es}']
+      ]
+    }
+  }, 'switch style to on regular field');
+
+  assert.deepEqual(esStyle.layers[1].layout, {
+    'text-letter-spacing': 0.15,
+    'text-field': {
+      'base': 1,
+      'stops': [
+        [0, '{abbr}'],
+        [6, '{name}']
+      ]
+    }
+  }, 'do not switch style on excluded field');
   assert.end();
 });
